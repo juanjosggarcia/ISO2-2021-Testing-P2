c7716b5d4eba5124b68221c1bb8b4b0e3211694a
juanjosggarcia <JuanJose.Garcia11@alu.uclm.es>
2021-12-18 16:08:23 +0100

    v.0.1.0

--- src/test/java/minions_SL/PlazasTransporte/AppTest.java
+++ src/test/java/minions_SL/PlazasTransporte/AppTest.java
@@ -17,5 +17,46 @@ public class AppTest
     {
         assertTrue( true );
     }
-
+    
+    @Test
+    public void obtenerNivelIATest1() {
+    	int esperado=-1;
+    	int actual=App.obtenerNivelIA(-500);
+		assertEquals(esperado, actual);
+    }
+    
+    @Test
+    public void obtenerNivelIATest2() {
+    	int esperado=0;
+    	int actual=App.obtenerNivelIA(50);
+		assertEquals(esperado, actual);
+    }
+    
+    @Test
+    public void obtenerNivelIATest3() {
+    	int esperado=1;
+    	int actual=App.obtenerNivelIA(150);
+		assertEquals(esperado, actual);
+    }
+    
+    @Test
+    public void obtenerNivelIATest4() {
+    	int esperado=2;
+    	int actual=App.obtenerNivelIA(250);
+		assertEquals(esperado, actual);
+    }
+    
+    @Test
+    public void obtenerNivelIATest5() {
+    	int esperado=3;
+    	int actual=App.obtenerNivelIA(350);
+		assertEquals(esperado, actual);
+    }
+    
+    @Test
+    public void obtenerNivelIATest6() {
+    	int esperado=4;
+    	int actual=App.obtenerNivelIA(1000);
+		assertEquals(esperado, actual);
+    }
 }
